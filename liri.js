17f3c8cb5239b80ab43b085abb248bd6302c850b
lambertco <blambertco@gmail.com>
2019-10-01 21:16:52 -0600

    spotify function written. search is throwing errors

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var spotify = spotify(keys.spotify);
 var request = require('request');
 var fs = require('fs');
 var Spotify = require('node-spotify-api');
@@ -18,10 +18,9 @@ var input = process.argv[3];
 
 function bandsInTown(bandQuery) {
 
-    // Then run a request to the OMDB API with the movie specified
+    // bands in town url
     var queryUrl = "https://rest.bandsintown.com/artists/" + bandQuery + "/events?app_id=codingbootcamp#";
-    // + movieQuery +
-    // This line is just to help us debug against the actual URL.
+  
     console.log(queryUrl);
 
     request(queryUrl, function (error, response, body) {
@@ -35,15 +34,46 @@ function bandsInTown(bandQuery) {
             var momentDT = moment().format('L');
 
 
-            console.log(concertData);
-            // for (i = 0; i < movieData.length && i < 5; i++) {
-          
-            // * Name of the venue
+            console.log(concertData); 
+            // Name of the venue
             console.log("Venue Name : " + concertData[0].venue.name +
-                // * Venue location
+                // Venue location
                 "\nVenue Location: " + concertData[0].venue.city + "," + concertData[0].venue.country +
-                //  * Date of the Event (use moment to format this as "MM/DD/YYYY")
+                //Date of the Event (use moment to format this as "MM/DD/YYYY")
                 "\nDate of the Event: " + momentDT);
         };
     });
 }
+
+function spotifyThis(musicSearch) {
+
+    //If no song is provided then your program will default to "The Sign" by Ace of Base.
+    if (musicSearch === undefined || null) {
+        musicSearch = "The Sign Ace of Base";
+    }
+
+    spotify.search({ type: 'track', query: musicSearch }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+                    
+        else {
+            for (i = 0; i < data.tracks.items.length && i < 5; i++){
+            
+                var musicQuery = data.tracks.items[i];
+                // console.log("===============================");
+                 // * Artist(s)
+                console.log("Artist: " + musicQuery.artists[0].name +
+                // * The song's name
+                "\nSong Name: " + musicQuery.name +
+                //* A preview link of the song from Spotify
+                "\nLink to Song: " + musicQuery.preview_url +
+                //* The album that the song is from
+                "\nAlbum Name: " + musicQuery.album.name +
+                "\n===============================");
+            }
+        };  
+    });
+}
+
+spotifyThis();
